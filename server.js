a101210bda46aafe2ae002054ef9dfca8b9c1b4f
yazan <yazanalshabki2001@gmail.com>
2024-05-21 11:06:01 +0300

    asd4

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 const port = 3000;
+const cors = require('cors');
+
+
+// view engine
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 let NAME;
 let START_TIME;
 let DURATION;
 let TIMEZONE;
-
 app.get("/", async (req, res) => {
   const code = req.query.code;
   try {
