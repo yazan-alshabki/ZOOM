8f5a88c6fc5dc47e3c040bd1c98d7419a1f550c5
yazan <yazanalshabki2001@gmail.com>
2024-05-22 12:38:43 +0300

    yazan2

--- server.js
+++ server.js
@@ -39,6 +39,9 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/auth/zoom", (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://zoom-p6sc.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   const clientId = process.env.ZOOM_API_KEY;
   const redirect_uri = encodeURIComponent(process.env.REDIRECT_URI);
   const responseType = "code";
