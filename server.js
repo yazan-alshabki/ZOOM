fe99c3f465b75726f289daa62cb1254387c312e3
yazan <yazanalshabki2001@gmail.com>
2024-05-22 12:46:01 +0300

    yazan3

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const cors = require('cors');
 
 const corsOptions = {
   origin: 'https://zoom-p6sc.onrender.com',
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true
 };
 
 // view engine
